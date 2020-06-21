d82cd532ed07c6fa2a6e85dcb4c220b49b008325
Eve <948832626@qq.com>
2020-06-21 16:18:51 +0800

    fix: delete DOM before page loaded to avoid useless DOM flashs

--- baidu/baidu.js
+++ baidu/baidu.js
@@ -1,23 +1,29 @@
 /**
  * FireFox extension config address: about:debugging#/runtime/this-firefox
- * Chrome extension config address: chrome://extensions/
+ * Chrome extension config address: chrome://extensions
  */
+
+
+/** If enable below code, then the code about DELETE will not work, idk why... */
+// // There is a css problem that search button on FireFox, let's fix it
+// const btnSearch = document.querySelector('#su');
+// btnSearch.style.height = '43.2px';
+
 // #region delete useless DOM
-const deleteIdList = [
+hideSelectors([
   '#s_top_wrap',
   '#bottom_layer',
   '#lm-new',
   '#s-top-left',
   '#u1',
   '#s-hotsearch-wrapper',
+  '.s-p-top',
   '#s_side_wrapper'
-];
-deleteIdList.forEach(v => {
-  const element = document.querySelector(v);
-  element.style.display = 'none';
-  // element.parentNode.removeChild(element);
-});
+]);
+
+function hideSelectors(v) {
+  const styleElement = document.createElement('style');
+  styleElement.textContent = v.join(',') + '{ display: none !important }';
+  document.documentElement.appendChild(styleElement);
+}
 // #endregion
-// There is a css problem that search button on FireFox, let's fix it
-const btnSearch = document.querySelector('#su');
-btnSearch.style.height = '43.2px';
