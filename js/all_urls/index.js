a12aa0b2456555254af47b00b3a5c7f06f47a3c1
Eve <948832626@qq.com>
2020-06-27 15:49:19 +0800

    feat: support dynamic query with mouse

--- js/all_urls/index.js
+++ js/all_urls/index.js
@@ -1,18 +1,17 @@
-$('body').mousemove(event => {
-  // Compatible with FireFox(only target) and Chrome(only path)
-  const currentElement = $(event.target || event.path[0]);
-  if (!hasCreate) {
+$('body')
+  .children()
+  .mouseover(event => {
+    // Compatible with FireFox(only target) and Chrome(only path)
+    const currentElement = $(event.target || event.path[0]);
     hightLightArea(currentElement);
-  }
-});
-hasCreate = false;
+  });
 
 /**
  * Highlight the area of the mouse's hover
  * @param {JQuery<HTMLElement>} dom A instance of DOM
  */
 function hightLightArea(dom) {
-  hasCreate = true;
+  // console.log(dom, `dom`);
   // #region Calculate basic attribute
   const top = dom.offset().top;
   const left = dom.offset().left;
@@ -20,15 +19,23 @@ function hightLightArea(dom) {
   const height = dom.outerHeight(true);
   // #endregion
   // #region Set attribute for selected dom
-  const selectedElem = $(
-    '<div id="cds-select-elem" style="position: absolute; background: #a0c5e8; opacity: 0.5; z-index: 999;"></div>'
-  );
+  const exist = $('#cds-select-elem').length;
+  let selectedElem;
+  if (exist) {
+    selectedElem = $('#cds-select-elem');
+  } else {
+    selectedElem = $(
+      '<div id="cds-select-elem" style="position: absolute; background: #a0c5e8; opacity: 0.5; z-index: 999;"></div>'
+    );
+  }
   selectedElem.css({
     width: `${width}px`,
     height: `${height}px`,
     left: `${left}px`,
     top: `${top}px`
   });
+  if (!exist) {
+    $('body').append(selectedElem);
+  }
   // #endregion
-  $('body').append(selectedElem);
 }
