b78398f48a44611120d3a98041bbe2683ac5f475
Eve <948832626@qq.com>
2020-07-12 16:58:52 +0800

    feat: send message between content-script and popup

--- js/select.js
+++ js/select.js
@@ -1,17 +1,48 @@
-// $('body')
-//   .children()
-//   .mouseover(event => {
-//     // Compatible with FireFox(only target) and Chrome(only path)
-//     const currentElement = $(event.target || event.path[0]);
-//     hightLightArea(currentElement);
-//   });
+function selectDOM() {
+  $('body').mousemove(event => {
+    //   let target = event.target;
+    //   while (target.parentNode) {
+    //     target = target.parentNode;
+    //     console.log(target, `target`);
+    //   }
+    //   $('body')
+    // .children().unbind();
+    // Compatible with FireFox(only target) and Chrome(only path)
+    const currentElement = $(event.target || event.path[0]);
+    deleteSelectArea();
+    hightLightArea(currentElement);
+    // console.log(currentElement);
+  });
+}
 
+function cancelSelectDOM() {
+  $('body').children().unbind();
+  deleteSelectArea();
+}
+
+function deleteSelectArea() {
+  $('#cds-select-elem').remove();
+}
+
+// Get message from popup or background;
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const { messageType, data } = request;
+  switch (messageType) {
+    case 'select-DOM':
+      const { action } = data;
+      if (action === 'open') {
+        selectDOM();
+      } else if (action === 'close') {
+        cancelSelectDOM();
+      }
+      break;
+  }
+});
 /**
  * Highlight the area of the mouse's hover
  * @param {JQuery<HTMLElement>} dom A instance of DOM
  */
 function hightLightArea(dom) {
-  console.log(dom, `dom`);
   // #region Calculate basic attribute
   const top = dom.offset().top;
   const left = dom.offset().left;
--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -1,3 +1,21 @@
-$('#setting-center').click(() =>
-  window.open(chrome.extension.getURL('./pages/background/background.html'))
-);
+$('#setting-center').click(() => {
+  window.open(chrome.extension.getURL('./pages/background/background.html'));
+});
+
+/**
+ * Send message to『content-script』
+ * @param {Object} request Data with JSON
+ */
+function sendMessage(request) {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, request, null);
+  });
+}
+
+$('#select-dom-open').click(() => {
+  sendMessage({ messageType: 'select-DOM', data: { action: 'open' } });
+});
+
+$('#select-dom-close').click(() => {
+  sendMessage({ messageType: 'select-DOM', data: { action: 'close' } });
+});
