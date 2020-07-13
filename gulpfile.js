028c8b30d05b51c85565031131c247dca1ebe08b
Eve <948832626@qq.com>
2020-07-13 21:51:03 +0800

    enhence: use gulp-newer

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
 /** If I change the commonJS to ESModule, the terminal shows 'Cannot use import statement outside a module', idk how to fix it */
 const { task, src, dest, watch, series, parallel } = require('gulp');
 const plugins = require('gulp-load-plugins')();
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
 
 function copyAllFiles(cb) {
-  src(['dev/**/*', '!dev/**/*.ts', '!dev/**/*.less']).pipe(dest('dist'));
+  src(['dev/**/*', '!dev/**/*.ts', '!dev/**/*.less'])
+    .pipe(plugins.newer('dist'))
+    .pipe(dest('dist'));
   cb();
 }
 
 function tsc(cb) {
-  src('dev/**/*.ts').pipe(tsProject()).pipe(dest('dist/'));
+  src('dev/**/*.ts')
+    .pipe(plugins.newer('dist'))
+    .pipe(tsProject())
+    .pipe(dest('dist'));
   cb();
 }
 
 // 对scss/less编译压缩，输出css文件
 function css(cb) {
   src('dev/**/*.less')
+    .pipe(plugins.newer('dist'))
     .pipe(plugins.less({ outputStyle: 'compressed' }))
     .pipe(
       plugins.autoprefixer({
@@ -34,7 +40,8 @@ function watcher() {
   watch('dev/', parallel([tsc, css]));
 }
 
-task('start', series([copyAllFiles, tsc, css, watcher]));
+task('start', series([tsc, css, watcher]));
+task('copy', parallel([copyAllFiles]));
 
 // It's unnesscery for me now, I'll use is when the project become very big
 // require('./scripts/start');
