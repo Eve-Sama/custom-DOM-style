3330f7b45747c32d6f839a2c278a9aecb21f284a
Eve <948832626@qq.com>
2020-07-22 22:44:26 +0800

    refactor: use style label instead of css

--- dev/content-script/init.ts
+++ dev/content-script/init.ts
@@ -14,25 +14,44 @@ const host = window.location.host;
 let styleStore: StyleStore[];
 
 // Inject script to page
-$(document).ready(function () {
+window.onload = function () {
   const script = document.createElement('script');
   script.setAttribute('type', 'text/javascript');
   script.src = chrome.extension.getURL('dist/content-script/inject.js');
   script.onload = e => $(e.target).remove();
   document.body.appendChild(script);
-});
+};
 
 chrome.storage.sync.get('cdsStyleStore', e => {
-  setTimeout(() => {
-    console.log(e, `e`);
-    const styleStore = e.cdsStyleStore as StyleStore[];
-    if (styleStore) {
-      const domainSetting = styleStore.find(v => v.host === host);
-      if (domainSetting) {
-        const { path, css } = domainSetting;
-        const dom = getDom(path);
-        applyDomSyle(dom, css);
-      }
+  // console.log(e, `e`);
+  const styleStore = e.cdsStyleStore as StyleStore[];
+  if (styleStore) {
+    const domainSetting = styleStore.find(v => v.host === host);
+    if (domainSetting) {
+      const { path, css } = domainSetting;
+      console.log(path, `path`);
+      console.log(css, `css`);
+      // const dom = getDom(path);
+      // applyDomSyle(dom, css);
+      let query = '';
+      path.forEach(v => {
+        if (v.id) {
+          query += `#${v.id}`;
+        }
+        if (v.cls) {
+          const classes = v.cls.split(' ');
+          let clsSelector = '';
+          classes.forEach(_cls => (clsSelector += `.${_cls}`));
+          query += clsSelector;
+        }
+        query += `:nth-child(${v.index}) `;
+      });
+      let cssText = '';
+      css.forEach(v => (cssText += `${v.key}:${v.value} !important;`));
+      console.log(query, `query`);
+      const el = document.createElement('style');
+      el.textContent = `${query}{ ${cssText} }`;
+      document.documentElement.appendChild(el);
     }
-  }, 1000);
+  }
 });
